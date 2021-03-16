f39ce7b235e219d97b968a37624dc795e94ecd03
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2021-03-16 18:53:20 +0000

    Token trasfer tests

--- src/backend/test/Setup.js
+++ src/backend/test/Setup.js
@@ -19,24 +19,41 @@ describe("Token contract", function () {
   describe("Deployment", function () {
     
     it('Should set the right owner', async function () {
-     expect(await token.owner()).to.equal(owner.address);
+        expect(await token.owner()).to.equal(owner.address);
     });
 
     it('Should set the contract name', async function () {
-     expect(await token.name()).to.equal('Token');
+        expect(await token.name()).to.equal('Token');
     })
 
     it('Should set the contract symbol', async function () {
-     expect(await token.symbol()).to.equal('ZRK');
+        expect(await token.symbol()).to.equal('ZRK');
+    })
+
+    it('Returns token total supply', async function () {
+        expect(await token.totalSupply()).to.equal(1000000);
     })
 
     it('Mints Tokens to the deployer', async function () {
-     const balance = await token.balanceOf(owner.address)
-     expect(balance).to.equal(1000000)
+        expect(await token.balanceOf(owner.address)).to.equal(1000000);
     })
   })
 
-  describe("", function () {
+  describe("Transferring Tokens", function () {
+    let amount = 1000000
+    // Transfer all tokens from owner to addr1
+    it('Transfers from contract owner', async function () {
+        let balance = await token.balanceOf(owner.address);   
+        await token.transfer(addr1.address, amount);
+        expect(await token.balanceOf(owner.address)).to.equal(0);
+        expect(await token.balanceOf(addr1.address)).to.equal(amount);
+    })
+
+    it('Transfers Tokens from owner', async function () {
+        await token.transfer(owner.address, amount), { from: addr1 };
+        expect(await token.balanceOf(owner.address)).to.equal(amount);
+        expect(await token.balanceOf(addr1.address)).to.equal(0);
+    })
     
-   })
+  })
 }) 
